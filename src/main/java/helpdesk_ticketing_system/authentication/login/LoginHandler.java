82a4810572dae7e4175bef79813e4ac848fefa59
sayantan <sayantandas657@outlook.com>
2023-04-04 23:17:35 +0530

    Modified to receive api gateway data and send proper response with status code

--- src/main/java/helpdesk_ticketing_system/authentication/login/LoginHandler.java
+++ src/main/java/helpdesk_ticketing_system/authentication/login/LoginHandler.java
@@ -2,28 +2,74 @@ package helpdesk_ticketing_system.authentication.login;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import helpdesk_ticketing_system.authentication.entities.RequestBody;
+import helpdesk_ticketing_system.authentication.entities.Response;
 import helpdesk_ticketing_system.authentication.service.Login;
 import helpdesk_ticketing_system.authentication.service.UsernamePasswordAuth;
 import helpdesk_ticketing_system.authentication.utility.CognitoClient;
+import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazon.awssdk.utils.StringUtils;
 
-import java.util.Map;
-
-public class LoginHandler implements RequestHandler<Map<String,String>,Object>
+public class LoginHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
 {
+    private final Gson gson;
     private final CognitoClient cognitoClient;
     private final Login usernamePasswordLoginAuth;
     public LoginHandler(){
+        this.gson = new GsonBuilder().setPrettyPrinting().create();
         this.cognitoClient = new CognitoClient();
         this.usernamePasswordLoginAuth = new UsernamePasswordAuth();
     }
 
     @Override
-    public Object handleRequest(Map<String, String> inputCredentials, Context context) {
-        return usernamePasswordLoginAuth.signIn(
-                cognitoClient,
-                inputCredentials.get("username"),
-                inputCredentials.get("password"),
-                context
-        );
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayInput, Context context) {
+        if(apiGatewayInput==null)
+            return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR);
+
+        String requestBodyJsonString = apiGatewayInput.getBody();
+        try{
+            RequestBody requestBody = gson.fromJson(requestBodyJsonString, RequestBody.class);
+
+            String username = requestBody.getUsername();
+            String password = requestBody.getPassword();
+
+            //both username and password values are present as expected
+            if(StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password))
+            {
+                Response response = usernamePasswordLoginAuth.signIn(cognitoClient, username, password, context);
+
+                //correct username or password
+                if(response.getStatus()==HttpStatusCode.OK)
+                    return new APIGatewayProxyResponseEvent()
+                            .withStatusCode(HttpStatusCode.OK)
+                            .withBody(gson.toJson(response));
+
+                //incorrect username or password
+                else if(response.getStatus()==HttpStatusCode.UNAUTHORIZED)
+                    return new APIGatewayProxyResponseEvent()
+                            .withStatusCode(HttpStatusCode.UNAUTHORIZED)
+                            .withBody(gson.toJson(response));
+            }
+
+            //username and/or password are not present
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(HttpStatusCode.BAD_REQUEST)
+                    .withBody("Username or Password cannot be Empty.");
+        }
+        catch (JsonSyntaxException e){ //malformed request body
+            context.getLogger().log("[ Error occurred ] " + e.getMessage() + "\n");
+            return new APIGatewayProxyResponseEvent()
+                    .withStatusCode(HttpStatusCode.BAD_REQUEST)
+                    .withBody("Malformed Request Body");
+        }
+        catch (Exception e ){ //any other errors occurs
+            context.getLogger().log("[ Error occurred ] " + e.getMessage() + "\n");
+        }
+        return new APIGatewayProxyResponseEvent().withStatusCode(HttpStatusCode.INTERNAL_SERVER_ERROR);
     }
 }
